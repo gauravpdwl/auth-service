1c5e8c0bc387a3d72ed3aedef4ca09e1b5581942
Gaurav <gauravpadwal45@gmail.com>
2024-06-05 22:10:19 +0530

    indentation changes due to prettier

--- scripts/generatekeys.mjs
+++ scripts/generatekeys.mjs
@@ -1,23 +1,23 @@
 /* eslint-disable no-undef */
-import crypto from 'crypto';
-import fs from 'fs';
+import crypto from "crypto";
+import fs from "fs";
 
-const {privateKey, publicKey }=crypto.generateKeyPairSync('rsa',{
-    modulusLength:2048,
-    publicKeyEncoding:{
-        type:'pkcs1',
-        format:'pem'
-    },
-    privateKeyEncoding:{
-        type:'pkcs1',
-        format:'pem'
-    }
-})
+const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+  modulusLength: 2048,
+  publicKeyEncoding: {
+    type: "pkcs1",
+    format: "pem",
+  },
+  privateKeyEncoding: {
+    type: "pkcs1",
+    format: "pem",
+  },
+});
 
 console.log("Public key", publicKey);
 console.log("Private Key ", privateKey);
 
-fs.writeFileSync('certs/private.pem', privateKey);
-fs.writeFileSync('certs/public.pem', publicKey);
+fs.writeFileSync("certs/private.pem", privateKey);
+fs.writeFileSync("certs/public.pem", publicKey);
 
 // execute using node filename to generate public, private files.
